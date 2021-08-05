dd33bd73a20f1f1ca419a294b446a5df908f4b5d
SM1701 <717513736@qq.com>
2021-08-05 09:44:59 +0800

    适配mysql8.0 && python3 语法规范

--- include.py
+++ include.py
@@ -21,7 +21,7 @@ TRX_UNDO_PAGE_HDR = FSEG_PAGE_DATA
 
 PAGE_LEVEL = 26 #level of the node in an index tree; the leaf level is the level 0 */
 
-innodb_page_type={
+EN_type={
         '0000':u'Freshly Allocated Page',
         '0002':u'Undo Log Page',
         '0003':u'File Segment inode',
@@ -34,8 +34,62 @@ innodb_page_type={
         '000a':u'Uncompressed BLOB Page',
         '000b':u'1st compressed BLOB Page',
         '000c':u'Subsequent compressed BLOB Page',
-        '45bf':u'B-tree Node'
+        '000d':u'Unknown',
+	'000e':u'Compressed page',
+	'000f':u'Encrypted page',
+	'0010':u'Compressed and Encrypted page',
+	'0011':u'Encrypted R-tree page',
+	'0012':u'Uncompressed SDI BLOB page',
+	'0013':u'Compressed SDI BLOB page',
+	'0014':u'Unused',
+	'0015':u'Rollback Segment Array page',
+	'0016':u'Index pages of uncompressed LOB(Large Objects)',
+	'0017':u'Data pages of uncompressed LOB(Large Objects)',
+	'0018':u'The first page of an uncompressed LOB(Large Objects)',
+	'0019':u'The first page of a compressed LOB(Large Objects)',
+	'001a':u'Data pages of compressed LOB(Large Objects)',
+	'001b':u'Index pages of compressed LOB(Large Objects)',
+	'001c':u'Fragment pages of compressed LOB(Large Objects)',
+	'001d':u'Index pages of fragment pages',
+	'45bf':u'B-tree Node',
+	'45be':u'R-tree Node',
+	'45bd':u'Tablespace SDI Index page'
         }
+
+CN_type={
+	'0000':u'新分配的页',
+	'0002':u'Undo 日志页',
+	'0003':u'文件段inode页',
+	'0004':u'插入缓冲空闲列表页',
+	'0005':u'插入缓冲位图',
+	'0006':u'系统页',
+	'0007':u'事务系统页',
+	'0008':u'文件空间头',
+	'0009':u'扩展说明页',
+	'000a':u'未压缩 BLOB 页',
+	'000b':u'压缩的 BLOB 字段的第一个页',
+	'000c':u'压缩的 BLOB 字段的页(非第一个)',
+	'000d':u'未知',
+	'000e':u'压缩页',
+	'000f':u'加密页',
+	'0010':u'加密+压缩的页',
+	'0011':u'加密的R-tree Ndoe',
+	'0012':u'解压的SDI blob 页',
+	'0013':u'压缩的SDI blob 页',
+	'0014':u'空闲页',
+	'0015':u'回滚段-数组页',
+	'0016':u'LOB(Large Objects)字段索引页',
+	'0017':u'LOB(Large Objects)字段数据页',
+	'0018':u'LOB(Large Objects)字段的第一个页',
+	'0019':u'LOB(Large Objects)(压缩)字段的第一个页',
+	'001a':u'LOB(Large Objects)(压缩)字段的数据页',
+	'001b':u'LOB(Large Objects)(压缩)字段索引页',
+	'001c':u'LOB(Large Objects)字段(压缩)的片段页',
+	'001d':u'LOB(Large Objects)字段(压缩)的片段页的索引页',
+	'45bf':u'B-tree 叶节点',
+	'45be':u'R-tree 叶节点',
+	'45bd':u'表空间的SDI索引页',
+	}
 
 innodb_page_direction={
         '0000': 'Unknown(0x0000)',
--- py_innodb_page_info.py
+++ py_innodb_page_info.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         if myargv.parse_cmdline() == 0:
                 pass
         else:
-                mylib.get_innodb_page_type(myargv)
+                mylib.get_innodb_page_type(myargv,True)
 
